bbf5e505f715db009ce187c08c6c3a78974d1473
Asuna Masuda <asuna.fitz@gmail.com>
2020-06-20 13:19:17 +0000

    Add home app and templates

---
+++ home/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views # need to import views from the current directory
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
