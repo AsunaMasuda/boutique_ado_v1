f5b8351961ed7faa94fab7657649dbf5f67498d4
Asuna Masuda <asuna.fitz@gmail.com>
2020-07-05 18:26:32 +0000

    complete webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -16,7 +16,7 @@ class OrderAdmin(admin.ModelAdmin):
     # These fields are all things that will be calculated by our model methods.
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total','original_bag', 'stripe_pid')
 
     # It will allow us to specify the order of the fields in the admin interface
     # which would otherwise be adjusted by django due to the use of some read-only fields.
@@ -25,7 +25,7 @@ class OrderAdmin(admin.ModelAdmin):
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
     # Last I'll use the list display option. 
     # To restrict the columns that show up in the order list to only a few key items.
